687a3e0357766cbfc1dac21faa4578c7b8961dbd
Joni Salonen <joni.salonen@gmail.com>
2021-06-26 10:26:31 -0400

    2020-05 add part2 answer to test

--- day05/part2_test.go
+++ day05/part2_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPart2(t *testing.T) {
 	actual := Part2()
-	expected := -1
+	expected := 705
 	if actual != expected {
 		t.Errorf("actual=%v expected=%v", actual, expected)
 	}
